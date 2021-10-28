ee0511921d3fc1e4e4e8e4f8b228ddcf40769627
unknown <nicolaslepetit@hotmail.fr>
2021-10-28 17:34:37 +0200

    fonction de filtre de la data par date, popularité et titre

--- src/js/modules/Filter.js
+++ src/js/modules/Filter.js
@@ -1,19 +1,32 @@
 import { getData } from "./getData.js"
 
 export const filterData = (filter) => {
-    let data = null
+    let data = {}
     console.log(filter)
     getData().then(result =>{
       console.log(result.media)
-      data = result.media.sort((a,b)=>{
-        if(filter === 'likes'){
-          return (a.date > b.date)
-        }
-        return a.price.localeCompare(b.price)
-      })
-      console.log(data)
+      if(filter === 'likes'){
+        console.log("LIKEEEES")
+        data = result.media.sort(function(a,b){
+          return b.likes - a.likes
+        })
+        console.log(data)
+      }
+      if(filter === 'date'){
+        console.log("DAAAAAAAATE")
+        data = result.media.sort(function(a,b){
+          return parseInt((a.date.match(/\d+/g)[0] + a.date.match(/\d+/g)[1] + a.date.match(/\d+/g)[2]),10) - parseInt((b.date.match(/\d+/g)[0] + b.date.match(/\d+/g)[1] + b.date.match(/\d+/g)[2]),10)
+        })
+        console.log(data)
+      }
+      if(filter === 'alttext'){
+        console.log("ALTTEEEEEXT")
+        data = result.media.sort(function(a,b){
+          return a.alttext.localeCompare(b.alttext)
+        })
+        console.log(data)
+      }
     })
-    console.log(data)
     return data
 }
 
