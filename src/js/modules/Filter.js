94c0e66bd9cbfbb51f7521ac731b7b732efee483
unknown <nicolaslepetit@hotmail.fr>
2021-11-02 12:56:57 +0100

    filter fonction popularité, date et titre

--- src/js/modules/Filter.js
+++ src/js/modules/Filter.js
@@ -1,35 +1,32 @@
 import { getData } from "./getData.js"
 
-export const filterData = (filter) => {
+export const filterData = async (filter) => {
     let data = {}
     console.log(filter)
-    getData().then(result =>{
-      console.log(result.media)
-      if(filter === 'likes'){
-        console.log("LIKEEEES")
-        data = result.media.sort(function(a,b){
-          return b.likes - a.likes
+    await getData().then(result =>{
+      console.log(result)
+      switch (filter) {
+        case 'likes' :
+          data.media = result.media.sort(function(a,b){
+            return b.likes - a.likes
+          })
+          data.photographers = result.photographers
+        break
+        case 'date' :
+          data.media = result.media.sort(function(a,b){
+            return parseInt((a.date.match(/\d+/g)[0] + a.date.match(/\d+/g)[1] + a.date.match(/\d+/g)[2]),10) - parseInt((b.date.match(/\d+/g)[0] + b.date.match(/\d+/g)[1] + b.date.match(/\d+/g)[2]),10)
         })
-        console.log(data)
-      }
-      if(filter === 'date'){
-        console.log("DAAAAAAAATE")
-        data = result.media.sort(function(a,b){
-          return parseInt((a.date.match(/\d+/g)[0] + a.date.match(/\d+/g)[1] + a.date.match(/\d+/g)[2]),10) - parseInt((b.date.match(/\d+/g)[0] + b.date.match(/\d+/g)[1] + b.date.match(/\d+/g)[2]),10)
-        })
-        console.log(data)
-      }
-      if(filter === 'alttext'){
-        console.log("ALTTEEEEEXT")
-        data = result.media.sort(function(a,b){
-          return a.alttext.localeCompare(b.alttext)
-        })
-        console.log(data)
+        data.photographers = result.photographers
+        break
+        case 'alttext' :
+          data.media = result.media.sort(function(a,b){
+            return a.alttext.localeCompare(b.alttext)
+          })
+          data.photographers = result.photographers
+        break
+        default: data = result
       }
     })
+    console.log(data)
     return data
 }
-
-// export const filterData = (string) => {
-//   console.log(string)
-// }
--- src/js/modules/singlePhotographer.js
+++ src/js/modules/singlePhotographer.js
@@ -1,94 +1,101 @@
 import { getData } from "./getData.js"
-// import { filterData } from "./Filter.js"
+import { filterData } from "./Filter.js"
+window.filterData = filterData
+// window.filterData = filterData
 
-try{
-    getData().then(result => {
+let filter = null
 
-      // GET URL ID
-      const url = window.location.href
-      const urlId = url.substring(url.lastIndexOf('=')+1)
-      const createDivForPhotographer = (photographer) => {
-        const myDiv = document.createElement("div")
-        myDiv.append(
-          createHeaderPhotographer(photographer),
-          createBodyPhotographer(photographer),
-          createFooterPhotographer(photographer)
-          )
-          return myDiv
+
+const singlePhotographer = (filter) => {
+
+  filterData(filter).then(result => {
+
+    // GET URL ID
+    const url = window.location.href
+    const urlId = url.substring(url.lastIndexOf('=')+1)
+    const createDivForPhotographer = (photographer) => {
+      const myDiv = document.createElement("div")
+      myDiv.setAttribute("id", "firstContent")
+      myDiv.append(
+        createHeaderPhotographer(photographer),
+        createBodyPhotographer(photographer),
+        createFooterPhotographer(photographer)
+        )
+        return myDiv
       }
 
       const createHeaderPhotographer = (photographer) => {
-          const myDiv = document.createElement("div")
-          const img = document.createElement("img")
-          img.src = `/src/img/${photographer.name}/${photographer.portrait}`
-          img.classList.add('photographerMiniature')
-          myDiv.appendChild(img)
-          return myDiv
+        const myDiv = document.createElement("div")
+        const img = document.createElement("img")
+        img.src = `/src/img/${photographer.name}/${photographer.portrait}`
+        img.classList.add('photographerMiniature')
+        myDiv.appendChild(img)
+        return myDiv
       }
 
       const createBodyPhotographer = (photographer) => {
-          const myDiv = document.createElement("div")
-          // CREATE TITLE
-          const title = document.createElement("h6")
-          title.innerHTML = photographer.name
+        const myDiv = document.createElement("div")
+        // CREATE TITLE
+        const title = document.createElement("h6")
+        title.innerHTML = photographer.name
 
-          // CREATE CITY
-          const city = document.createElement("p")
-          city.innerHTML = photographer.city
+        // CREATE CITY
+        const city = document.createElement("p")
+        city.innerHTML = photographer.city
 
-          // CREATE TAGLINE
-          const tagline = document.createElement('p')
-          tagline.innerHTML = photographer.tagline
+        // CREATE TAGLINE
+        const tagline = document.createElement('p')
+        tagline.innerHTML = photographer.tagline
 
-          //INSERT
-          myDiv.append(title, city, tagline)
+        //INSERT
+        myDiv.append(title, city, tagline)
 
-          return myDiv
+        return myDiv
       }
 
       const createFooterPhotographer = (photographer) => {
-          const myDiv = document.createElement("div")
-          myDiv.classList.add("flex")
+        const myDiv = document.createElement("div")
+        myDiv.classList.add("flex")
 
-          photographer.tags.forEach(tag => {
-            const tagElem = document.createElement("p")
-            tagElem.innerHTML = "#" + tag
-            myDiv.appendChild(tagElem)
-          })
-          return myDiv
+        photographer.tags.forEach(tag => {
+          const tagElem = document.createElement("p")
+          tagElem.innerHTML = "#" + tag
+          myDiv.appendChild(tagElem)
+        })
+        return myDiv
       }
 
       const createGalery = (medias) => {
-          const myDiv = document.createElement("div")
-          myDiv.classList.add("gridGalery")
-          medias.forEach(media => {
-            const imgDiv = document.createElement("div")
-            const img = document.createElement("img")
-            const underImg = document.createElement("div")
-            const title = document.createElement("h6")
-            const fav = document.createElement("p")
-            underImg.classList.add("flex", "justify-content-between", "align-items-center")
+        const myDiv = document.createElement("div")
+        myDiv.setAttribute("id", "gridGalery")
+        medias.forEach(media => {
+          const imgDiv = document.createElement("div")
+          const img = document.createElement("img")
+          const underImg = document.createElement("div")
+          const title = document.createElement("h6")
+          const fav = document.createElement("p")
+          underImg.classList.add("flex", "justify-content-between", "align-items-center")
 
-            img.src = `src/img/${getPhotographerById(media.photographerId).name}/${media.image}`
-            img.alt = media.alttext
-            title.innerHTML = media.alttext
-            fav.innerHTML = media.likes
-            underImg.append(title, fav)
-            imgDiv.append(img, underImg)
-            myDiv.appendChild(imgDiv)
-          })
-          return myDiv
+          img.src = `src/img/${getPhotographerById(media.photographerId).name}/${media.image}`
+          img.alt = media.alttext
+          title.innerHTML = media.alttext
+          fav.innerHTML = media.likes
+          underImg.append(title, fav)
+          imgDiv.append(img, underImg)
+          myDiv.appendChild(imgDiv)
+        })
+        return myDiv
       }
       const getPhotographerById = (id) => {
-          let getPhotographer = null
-          result.photographers.map(photographer => {
-            if(photographer.id === id){
-              console.log("photographer : ",photographer)
-              getPhotographer = photographer
-            }
-          })
-          console.log("getphotographer : ",getPhotographer)
-          return getPhotographer
+        let getPhotographer = null
+        result.photographers.map(photographer => {
+          if(photographer.id === id){
+            console.log("photographer : ",photographer)
+            getPhotographer = photographer
+          }
+        })
+        console.log("getphotographer : ",getPhotographer)
+        return getPhotographer
       }
       result.photographers.forEach(photographer => {
         // GET PHOTOGRAPHER PROFILE
@@ -97,13 +104,48 @@ try{
           content.appendChild(createDivForPhotographer(photographer))
         }
       })
-      let medias = result.media.filter(item => item.photographerId == urlId)
-      const galery = document.getElementById("galery")
-      galery.appendChild(createGalery(medias))
+    let medias = result.media.filter(item => item.photographerId == urlId)
+    const galery = document.getElementById("galery")
+    galery.appendChild(createGalery(medias))
 
 
 
-      })
-    } catch{
-      console.error("couldn't get data")
-    }
+  })
+}
+let singlePhotographerContent = document.getElementById("singlePhotographerContent")
+let galery = document.getElementById("galery")
+
+// POPULARITE FILTER
+document.getElementById("popuFilter").addEventListener("click", function(){
+  let firstContent = document.getElementById("firstContent")
+  let gridGalery = document.getElementById("gridGalery")
+  singlePhotographerContent.removeChild(firstContent)
+  galery.removeChild(gridGalery)
+  singlePhotographer('likes')
+})
+
+// DATE FILTER
+document.getElementById("dateFilter").addEventListener("click", function(){
+  let firstContent = document.getElementById("firstContent")
+  let gridGalery = document.getElementById("gridGalery")
+  singlePhotographerContent.removeChild(firstContent)
+  galery.removeChild(gridGalery)
+  singlePhotographer('date')
+})
+
+// TITLE FILTER
+document.getElementById("titleFilter").addEventListener("click", function(){
+  let firstContent = document.getElementById("firstContent")
+  let gridGalery = document.getElementById("gridGalery")
+  singlePhotographerContent.removeChild(firstContent)
+  galery.removeChild(gridGalery)
+  singlePhotographer('alttext')
+})
+
+console.log(filter)
+
+try{
+  singlePhotographer(filter)
+} catch(error){
+  console.error("couldn't get data", error)
+}
